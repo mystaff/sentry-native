dee67ecdfa12bb6a87ace38b63f2c22d699b0804
Nuno Luz <nuno.maluz@gmail.com>
2020-11-21 13:45:57 +0000

    make prepared http funcs public

--- src/sentry_transport.h
+++ src/sentry_transport.h
@@ -50,36 +50,4 @@ sentry_transport_t *sentry__transport_new_default(void);
 size_t sentry__transport_dump_queue(
     sentry_transport_t *transport, sentry_run_t *run);
 
-typedef struct sentry_prepared_http_header_s {
-    const char *key;
-    char *value;
-} sentry_prepared_http_header_t;
-
-/**
- * This represents a HTTP request, with method, url, headers and a body.
- */
-typedef struct sentry_prepared_http_request_s {
-    const char *method;
-    char *url;
-    sentry_prepared_http_header_t *headers;
-    size_t headers_len;
-    char *body;
-    size_t body_len;
-    bool body_owned;
-} sentry_prepared_http_request_t;
-
-/**
- * Consumes the given envelope and transforms it into into a prepared http
- * request. This can return NULL when all the items in the envelope have been
- * rate limited.
- */
-sentry_prepared_http_request_t *sentry__prepare_http_request(
-    sentry_envelope_t *envelope, const sentry_dsn_t *dsn,
-    const sentry_rate_limiter_t *rl);
-
-/**
- * Free a previously allocated HTTP request.
- */
-void sentry__prepared_http_request_free(sentry_prepared_http_request_t *req);
-
 #endif
