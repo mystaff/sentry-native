79ccafc88f77403afb70995c1ce1e8ff1a207f2f
Yucel Albar <yucel@timedoctor.com>
2025-01-31 15:30:43 +0300

    check if relaunch_argvw is null

--- src/backends/sentry_backend_crashpad.cpp
+++ src/backends/sentry_backend_crashpad.cpp
@@ -420,23 +420,28 @@ crashpad_backend_startup(
     std::vector<base::FilePath> attachments;
 
     sentry_path_t *current_exe = sentry__path_current_exe();
-    if (current_exe && options->relaunch_argv) {
-        annotations["__td-crashed-pid"] = std::to_string(td__getpid());
+    if (current_exe) {
+        if (options->relaunch_argv) {
+            annotations["__td-relaunch-argv"]
+                = std::string(options->relaunch_argv);
+            annotations["__td-relaunch-path"] = std::string(current_exe->path);
+        }
 #ifdef SENTRY_PLATFORM_WINDOWS
-        annotations["__td-relaunch-argv"]
-            = std::string { sentry__string_from_wstr(options->relaunch_argvw) };
-        annotations["__td-relaunch-path"]
-            = std::string { sentry__string_from_wstr(current_exe->path) };
-#else
-        annotations["__td-relaunch-argv"] = std::string(options->relaunch_argv);
-        annotations["__td-relaunch-path"] = std::string(current_exe->path);
+        if (options->relaunch_argvw) {
+            annotations["__td-relaunch-argv"] = std::string {
+                sentry__string_from_wstr(options->relaunch_argvw)
+            };
+            annotations["__td-relaunch-path"]
+                = std::string { sentry__string_from_wstr(current_exe->path) };
+        }
 #endif
+        annotations["__td-crashed-pid"] = std::to_string(td__getpid());
         sentry__path_free(current_exe);
     }
 
     // register attachments
     for (sentry_attachment_t *attachment = options->attachments; attachment;
-         attachment = attachment->next) {
+        attachment = attachment->next) {
         attachments.emplace_back(attachment->path->path);
     }
 
